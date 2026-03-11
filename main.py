2c9040ce2b094f126bb19ea448fa683472082698
Nirwan <nirwanmaharjantuladhar@gmail.com>
2026-03-11 19:27:31 +0545

    bst

---
+++ main.py
@@ -0,0 +1,34 @@
+class BSTNode:
+    def preorder(self, visited):
+        if self.val is not None:
+            visted.append(self.val)
+            preorder(self.val.left)
+            preorder(self.val.rigth)
+
+
+    # don't touch below this line
+
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+
+    def insert(self, val):
+        if not self.val:
+            self.val = val
+            return
+
+        if self.val == val:
+            return
+
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
+            self.left = BSTNode(val)
+            return
+
+        if self.right:
+            self.right.insert(val)
+            return
+        self.right = BSTNode(val)
